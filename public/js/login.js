92967e9a13a724a8271767b5b0378da705b4bdaf
jiamin <jih426@eng.ucsd.edu>
2018-02-23 00:17:33 -0800

    updated

--- public/js/login.js
+++ public/js/login.js
@@ -7,26 +7,26 @@ $(document).ready(function() {
   var $this = $(this),
       label = $this.prev('label');
 
-	  if (e.type === 'keyup') {
-			if ($this.val() === '') {
+    if (e.type === 'keyup') {
+      if ($this.val() === '') {
           label.removeClass('active highlight');
         } else {
           label.addClass('active highlight');
         }
     } else if (e.type === 'blur') {
-    	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
-			} else {
-		    label.removeClass('highlight');   
-			}   
+      if( $this.val() === '' ) {
+        label.removeClass('active highlight'); 
+      } else {
+        label.removeClass('highlight');   
+      }   
     } else if (e.type === 'focus') {
       
       if( $this.val() === '' ) {
-    		label.removeClass('highlight'); 
-			} 
+        label.removeClass('highlight'); 
+      } 
       else if( $this.val() !== '' ) {
-		    label.addClass('highlight');
-			}
+        label.addClass('highlight');
+      }
     }
 
 });
