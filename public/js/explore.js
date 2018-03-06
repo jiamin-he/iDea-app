b13ff31575ddd43cb6b7238077c97b67e223dea7
jiamin <jih426@eng.ucsd.edu>
2018-03-05 16:06:17 -0800

    update explore.js ga part

--- public/js/explore.js
+++ public/js/explore.js
@@ -18,57 +18,58 @@ function initializePage() {
     timestamp1 = new Date().getTime();
     console.log(timestamp1);
     ga('create','UA-93709601-3','auto');
+
+    $('#trendingSort').click(function(event){
+        timestamp2 = new Date().getTime();
+        // console.log(timestamp2);
+        ga('send', 'timing', 'add to my list', 'load', timestamp2-timestamp1);
+        event.preventDefault();
+        window.location ='/explore/trending';
+        // $.post('/explore', {section: "trending"}, postCallback);
+    });
+
+    $('#newestSort').click(function(event){
+        timestamp2 = new Date().getTime();
+        // console.log(timestamp2);
+        ga('send', 'timing', 'add to my list', 'load', timestamp2-timestamp1);
+        event.preventDefault();
+        window.location= '/explore/newest';
+        // $.post('/explore', {section: "newest"}, postCallback);
+    });
+
+    $('.fixed_bottom').click(function(event) {
+        timestamp2 = new Date().getTime();
+        ga('send', 'timing', 'add to my list', 'load', timestamp2-timestamp1);
+    });
+
+
+    $('.top_right_button').click(function(event){
+        timestamp2 = new Date().getTime();
+        ga('send', 'timing', 'add to my list', 'load', timestamp2-timestamp1);
+    });
+
+    $('.top_right_button_card').click(function(event){
+        
+        event.preventDefault();
+        // window.location.href = "/add_to_my_list";
+        var id = event.currentTarget.id
+        var isNewest = id.includes("newest")
+        id = isNewest? id.substr('top_right_button_newest_'.length):id.substr('top_right_button_trending_'.length);;
+        // console.log(event);
+        $.post('add_to_my_list', {id: id}, postCallback);
+        // $(this).find('i').toggleClass('fas fa-minus fa-lg')
+        // console.log($("#block-"+id));
+        var block = "#block-" +(isNewest?"newest-":"trending-")  + id;
+        // console.log(block);
+        $(block)[0].style.display = "none";
+
+        // for a/b testing
+        ga('send','event','add_to_my_list','click');
+
+    });
 }
 
 function postCallback(res){
     
   }
 
-$('#trendingSort').click(function(event){
-    timestamp2 = new Date().getTime();
-    // console.log(timestamp2);
-    ga('send', 'timing', 'add to my list', 'load', timestamp2-timestamp1);
-
-    window.location ='/explore/trending';
-    // $.post('/explore', {section: "trending"}, postCallback);
-});
-
-$('#newestSort').click(function(event){
-    timestamp2 = new Date().getTime();
-    // console.log(timestamp2);
-    ga('send', 'timing', 'add to my list', 'load', timestamp2-timestamp1);
-
-    window.location= '/explore/newest';
-    // $.post('/explore', {section: "newest"}, postCallback);
-});
-
-$('.fixed_bottom').click(function(event) {
-    timestamp2 = new Date().getTime();
-    ga('send', 'timing', 'add to my list', 'load', timestamp2-timestamp1);
-});
-
-
-$('.top_right_button').click(function(event){
-    timestamp2 = new Date().getTime();
-    ga('send', 'timing', 'add to my list', 'load', timestamp2-timestamp1);
-});
-
-$('.top_right_button_card').click(function(event){
-    
-    event.preventDefault();
-    // window.location.href = "/add_to_my_list";
-    var id = event.currentTarget.id
-    var isNewest = id.includes("newest")
-    id = isNewest? id.substr('top_right_button_newest_'.length):id.substr('top_right_button_trending_'.length);;
-    // console.log(event);
-    $.post('add_to_my_list', {id: id}, postCallback);
-    // $(this).find('i').toggleClass('fas fa-minus fa-lg')
-    // console.log($("#block-"+id));
-    var block = "#block-" +(isNewest?"newest-":"trending-")  + id;
-    // console.log(block);
-    $(block)[0].style.display = "none";
-
-    // for a/b testing
-    ga('send','event','add_to_my_list','click');
-
-});
